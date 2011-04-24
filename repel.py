0741a78486e63d5ee683392aa9c5ecc9a22060d6
Vincent Povirk <madewokherd@gmail.com>
2011-04-24 18:30:41 -0500

    Make bullets kill players.

--- repel.py
+++ repel.py
@@ -19,17 +19,29 @@ import pygame
 from pygame.locals import *
 
 PRECISION = 32
+MAX_BULLET_RADIUS = 2 << PRECISION
 
 class Object(object):
     x = 0
     y = 0
     dx = 0
     dy = 0
+    dead = False
     
     radius = 16 << PRECISION
     
     def sort_key(self):
         return (self.x, self.y)
+
+    def intersects(self, other):
+        if self.dead or other.dead:
+            return False
+    
+        max_distance_squared = (self.radius + other.radius)**2
+        
+        distance_squared = (self.x - other.x)**2 + (self.y - other.y)**2
+        
+        return max_distance_squared > distance_squared
 
 class Player(Object):
     radius = 16 << PRECISION
@@ -38,7 +50,6 @@ class Player(Object):
 
 class Bullet(Object):
     radius = 2 << PRECISION
-    dead = False
     
     pull = 1 << PRECISION
 
@@ -52,16 +63,70 @@ class World(object):
         self.height = height
         
         self.random = random.Random()
+
+    def find_bullet(self, obj):
+        min_index = 0
+        max_index = len(self.bullets)-1
+        
+        max_distance = obj.radius + MAX_BULLET_RADIUS
+        
+        min_x = obj.x - max_distance
+        max_x = obj.x + max_distance
+        
+        # search for any bullet with x in range
+        while max_index > min_index:
+            index = (max_index + min_index) // 2
+            
+            if min_x < self.bullets[index].x < max_x:
+                break
+            elif min_x > self.bullets[index].x:
+                min_index = index + 1
+            elif max_x < self.bullets[index].x:
+                max_index = index - 1
+        else:
+            # nothing with x in range
+            return None
+        
+        if self.bullets[index].intersects(obj):
+            return self.bullets[index]
+
+        i = index-1
+        while i >= 0 and self.bullets[i].x > min_x:
+            if self.bullets[i].intersects(obj):
+                return self.bullets[i]
+            i -= 1
+        
+        i = index+1
+        while i < len(self.bullets) and self.bullets[i].x < max_x:
+            if self.bullets[i].intersects(obj):
+                return self.bullets[i]
+            i += 1
+        
+        return None
         
     def advance(self):
         # destroy any out of range or used bullets
         for i in range(len(self.bullets)-1, -1, -1):
             bullet = self.bullets[i]
-            if bulllet.dead or bullet.x < 0 or bullet.y < 0 or bullet.x > self.width or bullet.y > self.height:
+            if bullet.dead or bullet.x < 0 or bullet.y < 0 or bullet.x > self.width or bullet.y > self.height:
                 self.bullets.pop(i)
 
         # check for collisions
+        self.bullets.sort(key=Object.sort_key)
         
+        for player in self.players:
+            bullet = self.find_bullet(player)
+            
+            if bullet is not None:
+                player.dead = True
+                bullet.dead = True
+        
+        # destroy any dead players/baddies
+        for i in range(len(self.players)-1, -1, -1):
+            player = self.players[i]
+            if player.dead:
+                self.players.pop(i)
+
         # move all the bullets
         for player in self.players:
             for bullet in self.bullets:
